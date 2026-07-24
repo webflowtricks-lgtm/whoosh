6fb69ddc6c211208c76df6186d1b163fbea0e415
robertosouza77 <95097772+robertosouza77@users.noreply.github.com>
2026-07-24 00:18:39 -0300

    a

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {ErrorBoundary} from './components/ErrorBoundary.tsx';
-import './lib/api.ts';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
